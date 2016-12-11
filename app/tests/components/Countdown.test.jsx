11a6a2d1934b4e825c4bd4ac27ca923616ebd1bb
Nanthakon Worachit <w.nanthakon@gmail.com>
2016-12-11 22:42:37 +0700

    Finalize tests & add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -7,11 +7,11 @@ var TestUtils = require('react-addons-test-utils')
 var Countdown = require('Countdown')
 
 describe('Countdown', () => {
-  it('should exist', () =>{
+  it('should exist', () => {
     expect(Countdown).toExist()
   })
 
-  describe('handleSetCountdown', () =>{
+  describe('handleSetCountdown', () => {
 
     it('should set state to started and countdown', (done) => {
       var countdown = TestUtils.renderIntoDocument(<Countdown/>)
@@ -20,7 +20,7 @@ describe('Countdown', () => {
       expect(countdown.state.count).toBe(10)
       expect(countdown.state.countdownStatus).toBe('started')
 
-      setTimeout(() =>{
+      setTimeout(() => {
         expect(countdown.state.count).toBe(9)
         done()
       }, 1001)
@@ -34,6 +34,30 @@ describe('Countdown', () => {
         expect(countdown.state.count).toBe(0)
         done()
       }, 3001)
+    })
+
+    it('should pause countdown on paused status', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>)
+      countdown.handleSetCountdown(3)
+      countdown.handleStatusChange('paused')
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3)
+        expect(countdown.state.countdownStatus).toBe('paused')
+        done()
+      }, 1001)
+    })
+
+    it('should reset count on stopped', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>)
+      countdown.handleSetCountdown(3)
+      countdown.handleStatusChange('stopped')
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0)
+        expect(countdown.state.countdownStatus).toBe('stopped')
+        done()
+      }, 1001)
     })
   })
 })
